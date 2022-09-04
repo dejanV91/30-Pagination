c475f27ce9e4d6b8692ee234a801556085926bed
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-04 22:04:30 +0200

    initial commit

--- paginate.js
+++ paginate.js
@@ -1,8 +1,8 @@
 const paginate = (followers) => {
     const itemsPerPage = 10;
     const numberOfPages = Math.ceil(followers.length / itemsPerPage);
-  
-    const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+
+    const newFollowers = Array.from({length: numberOfPages}, (_, index) => {
       const start = index * itemsPerPage
       return followers.slice(start, start + itemsPerPage)
     })
