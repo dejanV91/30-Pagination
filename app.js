a73189b4bb9c11c43a9470f45cbfd8016a2e2b2f
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-06 22:08:06 +0200

    initial commit

--- app.js
+++ app.js
@@ -5,39 +5,41 @@ import displayButtons from './displayButtons.js'
 const title = document.querySelector('.section-title h1')
 const btnContainer = document.querySelector('.btn-container')
 
-let index = 0
-let pages = []
+let index = 0;
+let pages = [];
 
-const setupUI = () => {
-  displayFollowers(pages[index])
-  displayButtons(btnContainer, pages, index)
+const setUi = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
 }
 
 const init = async () => {
-  const followers = await fetchFollowers()
-  title.textContent = 'pagination'
-  pages = paginate(followers)
-  setupUI()
+  const followers = await fetchFollowers();
+  paginate(followers);
+  title.textContent = "pagination";
+  pages = paginate(followers);
+  setUi(followers);
 }
 
-btnContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('btn-container')) return
-  if (e.target.classList.contains('page-btn')) {
-    index = parseInt(e.target.dataset.index)
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("page-btn")){
+    index = e.target.dataset.index;
   }
-  if (e.target.classList.contains('next-btn')) {
-    index++
+  if (e.target.classList.contains("next-btn")) {
+    index++;
     if (index > pages.length - 1) {
-      index = 0
+      index = 0;
     }
   }
-  if (e.target.classList.contains('prev-btn')) {
-    index--
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
     if (index < 0) {
-      index = pages.length - 1
+      index = pages.length - 1;
     }
   }
-  setupUI()
+  setUi();
 })
 
-window.addEventListener('load', init)
+window.addEventListener("load", init);
+
+
